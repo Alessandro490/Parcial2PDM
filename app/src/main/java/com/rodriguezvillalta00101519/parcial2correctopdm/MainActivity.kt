d3a83c65db7de2b5fec6c6ee433d8c21ac6b962e
Alessandro490 <00101519@example.com>
2023-05-23 09:05:49 -0600

    Created Project

---
+++ app/src/main/java/com/rodriguezvillalta00101519/parcial2correctopdm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rodriguezvillalta00101519.parcial2correctopdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
