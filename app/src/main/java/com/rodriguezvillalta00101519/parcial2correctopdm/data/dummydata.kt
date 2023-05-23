2471604fbb3069763596b0c212950a41a734f8f2
Alessandro490 <00101519@example.com>
2023-05-23 09:49:57 -0600

    Capa Modelo

---
+++ app/src/main/java/com/rodriguezvillalta00101519/parcial2correctopdm/data/dummydata.kt
@@ -0,0 +1,17 @@
+package com.rodriguezvillalta00101519.parcial2correctopdm.data
+
+import com.rodriguezvillalta00101519.parcial2correctopdm.data.models.ActressModel
+
+
+val name = "Xiaomi"
+val age = "60"
+val description = "A dynamic model"
+
+val name2 = "Poco"
+val age2 = "X3"
+val description2 = "A new quality-price phnoe"
+
+val actress= mutableListOf(
+    ActressModel(name, age, description),
+    ActressModel(name2, age2, description2)
+)
---
+++ app/src/main/java/com/rodriguezvillalta00101519/parcial2correctopdm/data/models/ActressModel.kt
@@ -0,0 +1,7 @@
+package com.rodriguezvillalta00101519.parcial2correctopdm.data.models
+
+data class ActressModel (
+    val name: String,
+    val age: String,
+    val description: String,
+    )
